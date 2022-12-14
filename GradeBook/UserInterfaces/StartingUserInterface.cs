8156d23265051f3167b487c07b09f9697a51b4a7
ian6ft5 <irogers@bullfrogspas.com>
2022-12-14 13:25:58 -0700

    5: bug fix in switch statement default

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,7 +41,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2];
-            BaseGradeBook gradeBook;            }
+            BaseGradeBook gradeBook;
             if (type == "standard")
             {
                 gradeBook = new StandardGradeBook(name);
